92614c2571c7dd60f95816929c559b04038eab6e
John Comeau <jcomeau@unternet.net>
2023-11-29 17:27:51 +0100

    first attempt

---
+++ lzw.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+'''
+Simple LZW decoder for PDF reverse engineering
+'''
+import sys, logging
+
+CLEAR_CODE = 256
+END_OF_INFO_CODE = 257
+GLOBAL = {
+    'bitlength': 9,
+}
+
+logging.basicConfig(level=logging.DEBUG if __debug__ else logging.WARNING)
+
+def nextcode(filename):
+    '''
+    get next code from lzw-compressed data
+
+    requires Python 3.8 or better for 'walrus' (:=) operator
+    '''
+    instream = ''
+    with open(filename, 'rb') as infile:
+        while byte := infile.read(1):
+            rawbyte = ord(byte)
+            logging.debug('input byte %s: 0x%x', byte, rawbyte)
+            instream += format(rawbyte, '08b')
+            if len(instream) >= GLOBAL['bitlength']:
+                code = instream[:GLOBAL['bitlength']]
+                logging.debug('next code: %s', code)
+                instream = instream[GLOBAL['bitlength']:]
+                yield int(code, 2)
+
+def newdict(specialcodes=True):
+    '''
+    build clean starting dict for LZW decompression
+
+    `specialcodes` means that the CLEAR_CODE (256) and END_OF_INFO_CODE (257)
+    from https://github.com/joeatwork/python-lzw are in use, which is the
+    case in the data extracted from my Red Cross certification card PDF.
+    '''
+    codedict = {k: bytes([k]) for k in range(256)}
+    if specialcodes:
+        codedict[CLEAR_CODE] = None
+        codedict[END_OF_INFO_CODE] = None
+    #logging.debug('codedict: %s', codedict)
+    return codedict
+
+def decode(filename, specialcodes=True):
+    codegen = nextcode(filename)
+    codedict = newdict(specialcodes)
+    decompressed = []
+    with open(filename + '.dat', 'wb') as outfile:
+        lastvalue = codevalue = None
+        for code in codegen:
+            try:
+                codevalue = codedict[code]
+            except KeyError:  # code wasn't in dict
+                codevalue = lastvalue + lastvalue[0:1]
+            if codevalue is not None:
+                outfile.write(codevalue)
+                #outfile.flush()  # in case of error down the line
+                lastkey = len(codedict) - 1
+                try:
+                    codedict[lastkey + 1] = lastvalue + codevalue[0]
+                    logging.debug('added %d: %s to codedict', lastkey + 1,
+                                  codedict[lastkey + 1])
+                except TypeError:  # very first byte output has no lastvalue
+                    logging.debug('not adding anything to dict after first'
+                                  ' output byte %s', codevalue)
+                    pass
+                if (lastkey + 2).bit_length() > lastkey.bit_length():
+                    GLOBAL['bitlength'] += 1
+                lastvalue = codevalue
+            else:  # CLEAR_CODE or END_OF_INFO_CODE
+                logging.debug('special code found, resetting dictionary')
+                codedict.clear()
+                codedict.update(newdict(specialcodes))
+                GLOBAL['bitlength'] = 9
+                if code == END_OF_INFO_CODE:
+                    return
+
+if __name__ == '__main__':
+    print(decode(sys.argv[1]))
