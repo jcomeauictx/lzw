ce8b0332124d6092f8633ef152ccf716a121555c
John Comeau <jcomeau@unternet.net>
2023-12-02 04:38:20 +0100

    gives same result as lzw.py!?

--- lzw.cs
+++ lzw.cs
@@ -1,7 +1,7 @@
 #!/usr/src/jcomeauictx/casperscript/bin/bccs
 (card.lzw) (r) file dup bytesavailable (bytes available: ) print ==
 /LZWDecode filter (pstack:) == pstack
-1024 string /stringbuffer def
-(%stdout) (w) file /outfile def
-{dup stringbuffer readstring over length 0 gt or
-  {outfile writestring} {exit} ifelse} loop
+/stringbuffer 1024 string def
+/outfile (card.rgb.check) (w) file def
+{dup stringbuffer readstring 1 index length 0 gt or
+  {outfile exch writestring} {pop exit} ifelse} loop
