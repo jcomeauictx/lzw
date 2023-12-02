1c93eace39c34c8af38f9e395bfbf18e9269c0fd
John Comeau <jcomeau@unternet.net>
2023-12-02 12:38:35 +0100

    write verbose stuff to stderr

--- lzw.cs
+++ lzw.cs
@@ -1,9 +1,17 @@
 #!/usr/src/jcomeauictx/casperscript/bin/bccs
 /infile argc 1 gt {argv 1 get} {(/dev/stdin)} ifelse def
 /outfile argc 2 gt {argv 2 get} {(/dev/stdout)} ifelse def
-infile (r) file dup bytesavailable (bytes available: ) print ==
-/LZWDecode filter (pstack:) == pstack
+infile (r) file
+dup bytesavailable [0] astore
+128 string (bytes of LZW data: %d\n) 3 -1 roll sprintf
+{(%stderr) (w) file exch writestring} {pop} ifelse
+/LZWDecode filter
 /stringbuffer 1024 string def
 /output outfile (w) file def
 {dup stringbuffer readstring 1 index length 0 gt or
-  {output exch writestring} {pop exit} ifelse} loop
+  {output exch writestring} {pop output flushfile exit} ifelse} loop
+[ outfile status ] 1 get dup 0 gt
+  {[0] astore 128 string (bytes written: %d) 3 -1 roll sprintf
+    {(%stderr) (w) file exch writestring} {pop} ifelse}
+  {pop}
+  ifelse
