bb6b3997a162c302ce0466cb026f71fa1783a940
John Comeau <jc@unternet.net>
2024-04-17 23:31:04 -0700

    new class not yet housebroken

--- lzwfilter.py
+++ lzwfilter.py
@@ -15,6 +15,9 @@ MAXBITS = 12
 # encoding and decoding tables, make shallow copies of these for use
 STRINGTABLE = {n: bytes([n]) for n in range(256)}  # codes to strings
 CODETABLE = dict(map(reversed, STRINGTABLE.items()))  # strings to codes
+CLEAR_CODE = 256  # see TIFF6.pdf pp. 58-63 for use of special codes
+END_OF_INFO_CODE = 257
+SPECIAL = {CLEAR_CODE: None, END_OF_INFO_CODE: None}
 
 class CodeReader(io.BufferedReader):
     r'''
@@ -52,7 +55,7 @@ class CodeReader(io.BufferedReader):
             if not nextbytes:  # empty string or None: EndOfFile
                 if not self.bitstream:
                     raise StopIteration
-                self.bitstream.extend(
+                self.bitstream.extend(  # pad remaining bits with zeroes
                     b'0' * (self.bitlength - len(self.bitstream)))
             else:
                 for byte in nextbytes:
@@ -75,6 +78,173 @@ class CodeReader(io.BufferedReader):
                 break
         return result
 
+class LZWReader(CodeReader):
+    '''
+    Implementation of LZW decompressor
+ 
+    adapted from pseudocode on page 61 of TIFF6.pdf
+
+        while ((Code = GetNextCode()) != EoiCode) {
+            if (Code == ClearCode) {
+                InitializeTable();
+                //Code = GetNextCode();  // NOTE: we don't do this here,
+                //if (Code == EoiCode)   // let next loop handle it.
+                //   break;
+                //WriteString(StringFromCode(Code));
+                //OldCode = Code;
+            } /* end of ClearCode case */
+            else {
+                if (IsInTable(Code)) {
+                    WriteString(StringFromCode(Code));
+                    AddStringToTable(StringFromCode(OldCode) +
+                        FirstChar(StringFromCode(Code)));
+                    OldCode = Code;
+                } else {
+                    OutString = StringFromCode(OldCode) +
+                        FirstChar(StringFromCode(OldCode));
+                    WriteString(OutString);
+                    AddStringToTable(OutString);
+                    OldCode = Code;
+                }
+            } /* end of not-ClearCode case */
+        } /* end of while loop */
+
+    let's simplify the nested if-else above for DRY:
+
+                if (IsInTable(Code)) {
+                    OutString = StringFromCode(Code);
+                    StoreString = StringFromCode(OldCode) +
+                        FirstChar(StringFromCode(Code);
+                } else {
+                    OutString = StringFromCode(OldCode) +
+                        FirstChar(StringFromCode(OldCode);
+                    StoreString = OutString;
+                }
+                WriteString(OutString);
+                AddStringToTable(StoreString);
+                OldCode = Code;
+
+    much better, except that since we ignore the suggestion in the
+    original pseudocode to process separately the first code after ClearCode,
+    there will be an exception trying to access OldCode for the first
+    in-table Code. We handle it by setting StoreString to null (None) in
+    that case. So the complete rewritten pseudocode would be:
+            
+        while ((Code = GetNextCode()) != EoiCode) {
+            if (Code == ClearCode) {
+                InitializeTable();
+            } /* end of ClearCode case */
+            else {
+                if (IsInTable(Code)) {
+                    OutString = StringFromCode(Code);
+                    try {
+                        StoreString = StringFromCode(OldCode) +
+                            FirstChar(StringFromCode(Code);
+                    } except(NoOldCodeImmediatelyAfterClearCode) {
+                        StoreString = null;
+                    }
+                } else {
+                    OutString = StringFromCode(OldCode) +
+                        FirstChar(StringFromCode(OldCode);
+                    StoreString = OutString;
+                }
+                WriteString(OutString);
+                if (StoreString != null) AddStringToTable(StoreString);
+                OldCode = Code;
+            }
+        }
+
+    Test cases from https://rosettacode.org/wiki/LZW_compression
+    >>> decode = lambda codes: LZWReader(BytesIO(codes)).read()
+    >>> codes = [84,79,66,69,79,82,78,79,84,256,258,260,265,259,261,263]
+    >>> decode(codes)
+    b'TOBEORNOTTOBEORTOBEORNOT'
+    >>> codes = [84,111,32,98,101,32,111,114,32,110,111,116,32,116,257,259,
+    ...     268,104,97,267,105,115,272,260,113,117,101,115,116,105,111,110,33]
+    >>> decode(codes)
+    b'To be or not to be that is the question!'
+    >>> codes = [34,84,104,101,114,101,32,105,115,32,110,111,116,104,
+    ...     105,110,103,32,112,259,109,97,110,101,110,116,32,101,120,
+    ...     99,101,112,281,99,104,277,103,101,46,34,32,296,45,298,296,
+    ...     32,72,259,97,99,108,105,116,117,264,32,91,53,52,48,32,299,
+    ...     52,55,53,32,66,67,69,93]
+    >>> check = decode(codes)
+    >>> check.startswith(b'"There is nothing permanent except change."')
+    True
+    >>> check.index(b'---   Heraclitus  [540 -- 475 BCE]')
+    46
+    '''
+    def __init__(self, stream, buffer_size=BUFFER_SIZE,
+                 minbits=MINBITS, maxbits=MAXBITS):
+        super().__init__(stream, buffer_size, minbits, maxbits)
+        self.codedict = dict(SPECIAL).update(STRINGTABLE)
+        self.oldcode = None
+        self.buffer = bytearray()
+
+    def __next__(self):
+        '''
+        returns next string from table, adjusting bitlength as we go
+        '''
+        # while ((Code = GetNextCode()) != EoiCode) {
+        code = next(super())
+        if code == END_OF_INFO_CODE:
+            raise StopIteration
+        #    if (Code == ClearCode) {
+        #        InitializeTable();
+        if code == CLEAR_CODE:
+            self.codedict = dict(SPECIAL).update(STRINGTABLE)
+        #    else {
+        #        if (IsInTable(Code)) {
+        #            OutString = StringFromCode(Code);
+        if code in self.codedict:
+            outstring = self.codedict[code]
+        #            try {
+        #                StoreString = StringFromCode(OldCode) +
+        #                    FirstChar(StringFromCode(Code);
+        #            } except(NoOldCodeImmediatelyAfterClearCode) {
+        #                StoreString = null;
+        #            }
+            try:
+                storestring = self.codedict[self.oldcode] + outstring[0:1]
+            except KeyError:
+                storestring = None
+        #        } else {
+        #            OutString = StringFromCode(OldCode) +
+        #                FirstChar(StringFromCode(OldCode);
+        #            StoreString = OutString;
+        #        }
+        else:
+            outstring = self.codedict[oldcode]
+            outstring += outstring[0:1]
+            storestring = outstring
+        #        WriteString(OutString);
+        #        if (StoreString != null) AddStringToTable(StoreString);
+        #        OldCode = Code;
+        #    }
+        # }
+        self.add_string_to_table(storestring)
+        self.oldcode = code
+        return outstring
+
+    def read(self, count=None):
+        '''
+        Return `count` bytes, defaulting to all available
+        '''
+        count = count or sys.maxsize
+        offset = 0
+        while len(self.buffer) < count:
+            try:
+                self.buffer.extend(next(self))
+            except StopIteration:
+                break
+        result = bytes(self.buffer[:count])
+        self.buffer[:count] = []
+        return result
+
+    def add_string_to_table(string):
+        newkey = len(self.codetable)
+        self.codetable[key] = string
+                
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
