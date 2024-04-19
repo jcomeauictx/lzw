7dbf9b6d6eba8c52eb49b16171ee8139a21240dd
John Comeau <jc@unternet.net>
2024-04-19 08:20:19 -0700

    going to figure this out

--- supertest.py
+++ supertest.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python3
+'''
+For figuring out how `super` works
+
+see https://stackoverflow.com/a/78351285/493161
+'''
 class A:
     msg = 'hohoho'
 class B(A):
     msg = 'hahaha'
 class C(B):
     msg = 'hehehe'
-c = C()
-print(c.msg)
-print(super(C, c).msg)
-print(super(B, c).msg)
+
+if __name__ == '__main__':
+    c = C()
+    print(c.msg)
+    print(super(C, c).msg)
+    print(super(B, c).msg)
