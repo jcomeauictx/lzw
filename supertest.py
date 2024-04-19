bd4931374eea078f966af460a621227a2e415c1a
John Comeau <jc@unternet.net>
2024-04-19 08:27:33 -0700

    AttributeError: 'super' object has no attribute 'message'

--- supertest.py
+++ supertest.py
@@ -4,8 +4,14 @@ For figuring out how `super` works
 
 see https://stackoverflow.com/a/78351285/493161
 '''
+import logging
+
+logging.basicConfig(level=logging.DEBUG if __debug__ else logging.WARNING)
+
 class A:
     msg = 'hohoho'
+    def __init__(self, message=msg):
+        self.message = message
 class B(A):
     msg = 'hahaha'
 class C(B):
@@ -13,6 +19,11 @@ class C(B):
 
 if __name__ == '__main__':
     c = C()
-    print(c.msg)
-    print(super(C, c).msg)
+    b = super(C, c)
+    a = super(B, c)
+    logging.debug('C class msg: %s, message: %s', c.msg, c.message)
+    logging.debug('C parent class msg: %s, message: %s', b.msg, b.message)
+    logging.debug('C grandparent class msg: %s, message: %s', a.msg, a.message)
+    print(c.message)
+    print(super(C, c).message)
     print(super(B, c).msg)
