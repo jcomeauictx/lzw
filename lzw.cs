4af1a9ac165495e605ce7869bb99bf47d2b4cc08
John Comeau <jc@unternet.net>
2024-05-06 16:26:19 -0700

    refactoring in progress

--- lzw.cs
+++ lzw.cs
@@ -3,14 +3,14 @@
 /infile sys.argc 2 gt {sys.argv 2 get} {(/dev/stdin)} ifelse def
 /outfile sys.argc 3 gt {sys.argv 3 get} {(/dev/stdout)} ifelse def
 action (decode) eq {
-  infile (r) file
-  dup bytesavailable [0] astore
+  /input infile (r) file def
+  input bytesavailable [0] astore
   128 string (bytes of LZW data: %d\n) 3 -1 roll sprintf
   {(%stderr) (w) file exch writestring} {pop} ifelse
-  /LZWDecode filter
+  /input input /LZWDecode filter def
   /stringbuffer 1024 string def
   /output outfile (w) file def
-  {dup stringbuffer readstring 1 index length 0 gt or
+  {input stringbuffer readstring 1 index length 0 gt or
     {output exch writestring} {pop output flushfile exit} ifelse} loop
   [ outfile status ] 1 get dup 0 gt
     {[0] astore 128 string (bytes written: %d\n) 3 -1 roll sprintf
