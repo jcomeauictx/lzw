0c2c20700f1263b34ea1fdeaa0e9e9666fca43a5
John Comeau <jc@unternet.net>
2024-05-06 19:23:57 -0700

    don't use the Python programs, the casperscript is much faster

--- lzw.cs
+++ lzw.cs
@@ -3,13 +3,13 @@
 /infile sys.argc 2 gt {sys.argv 2 get} {(/dev/stdin)} ifelse def
 /outfile sys.argc 3 gt {sys.argv 3 get} {(/dev/stdout)} ifelse def
 action (decode) eq {
-  /input infile (r) file def
-  input bytesavailable [0] astore
-  128 string (bytes of LZW data: %d\n) 3 -1 roll sprintf
+  /input infile (r) file def /input /input.orig alias
+  /output outfile (w) file def /output /output.orig alias
+  input.orig bytesavailable [0] astore
+  128 string (bytes of input data: %d\n) 3 -1 roll sprintf
   {(%stderr) (w) file exch writestring} {pop} ifelse
   /input input /LZWDecode filter def
   /stringbuffer 1024 string def
-  /output outfile (w) file def
   {input stringbuffer readstring 1 index length 0 gt or
     {output exch writestring} {pop output flushfile exit} ifelse} loop
   [ outfile status ] 1 get dup 0 gt
@@ -19,12 +19,15 @@ action (decode) eq {
     ifelse
 }
 {  % encode
-  /input infile (r) file def
-  /output outfile (w) file def
-  /compressed output /LZWEncode filter def
+  /input infile (r) file def /input /input.orig alias
+  /output outfile (w) file def /output /output.orig alias
+  input.orig bytesavailable [0] astore
+  128 string (bytes of input data: %d\n) 3 -1 roll sprintf
+  {(%stderr) (w) file exch writestring} {pop} ifelse
+  /output output /LZWEncode filter def
   /stringbuffer 1024 string def
   {input stringbuffer readstring 1 index length 0 gt or
-    {compressed exch writestring} {compressed flushfile exit} ifelse} loop
+    {output exch writestring} {output flushfile exit} ifelse} loop
   [ outfile status ] 1 get dup 0 gt
     {[0] astore 128 string (bytes written: %d\n) 3 -1 roll sprintf
       {(%stderr) (w) file exch writestring} {pop} ifelse}
