ff8caa6bdd607b9c9d4ad3b7d988a543da73d033
John Comeau <jc@unternet.net>
2024-05-06 19:32:33 -0700

    refactoring complete, for now anyway.

--- lzw.cs
+++ lzw.cs
@@ -2,35 +2,20 @@
 /action sys.argc 1 gt {sys.argv 1 get} {(decode)} ifelse def
 /infile sys.argc 2 gt {sys.argv 2 get} {(/dev/stdin)} ifelse def
 /outfile sys.argc 3 gt {sys.argv 3 get} {(/dev/stdout)} ifelse def
-action (decode) eq {
-  /input infile (r) file def /input /input.orig alias
-  /output outfile (w) file def /output /output.orig alias
-  input.orig bytesavailable [0] astore
-  128 string (bytes of input data: %d\n) 3 -1 roll sprintf
-  {(%stderr) (w) file exch writestring} {pop} ifelse
-  /input input /LZWDecode filter def
-  /stringbuffer 1024 string def
-  {input stringbuffer readstring 1 index length 0 gt or
-    {output exch writestring} {pop output flushfile exit} ifelse} loop
-  [ outfile status ] 1 get dup 0 gt
-    {[0] astore 128 string (bytes written: %d\n) 3 -1 roll sprintf
-      {(%stderr) (w) file exch writestring} {pop} ifelse}
-    {pop}
-    ifelse
-}
-{  % encode
-  /input infile (r) file def /input /input.orig alias
-  /output outfile (w) file def /output /output.orig alias
-  input.orig bytesavailable [0] astore
-  128 string (bytes of input data: %d\n) 3 -1 roll sprintf
-  {(%stderr) (w) file exch writestring} {pop} ifelse
-  /output output /LZWEncode filter def
-  /stringbuffer 1024 string def
-  {input stringbuffer readstring 1 index length 0 gt or
-    {output exch writestring} {output flushfile exit} ifelse} loop
-  [ outfile status ] 1 get dup 0 gt
-    {[0] astore 128 string (bytes written: %d\n) 3 -1 roll sprintf
-      {(%stderr) (w) file exch writestring} {pop} ifelse}
-    {pop}
-    ifelse
-} ifelse
+/input infile (r) file def /input /input.orig alias
+/output outfile (w) file def /output /output.orig alias
+input.orig bytesavailable [0] astore
+128 string (bytes of input data: %d\n) 3 -1 roll sprintf
+{(%stderr) (w) file exch writestring} {pop} ifelse
+action (decode) eq
+  {/input input /LZWDecode filter def}
+  {/output output /LZWEncode filter def}
+  ifelse
+/stringbuffer 1024 string def
+{input stringbuffer readstring 1 index length 0 gt or
+{output exch writestring} {pop output flushfile exit} ifelse} loop
+[ outfile status ] 1 get dup 0 gt
+  {[0] astore 128 string (bytes written: %d\n) 3 -1 roll sprintf
+    {(%stderr) (w) file exch writestring} {pop} ifelse}
+  {pop}
+ifelse
